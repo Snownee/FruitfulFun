f5ed0905116d5bba7f61f91dc6773afc9ef560ee
Snownee <1850986885@qq.com>
2020-01-26 02:25:00 +0800

    Fix wrong logger

--- src/main/java/snownee/fruits/mixin/Connector.java
+++ src/main/java/snownee/fruits/mixin/Connector.java
@@ -3,14 +3,14 @@ package snownee.fruits.mixin;
 import org.spongepowered.asm.mixin.Mixins;
 import org.spongepowered.asm.mixin.connect.IMixinConnector;
 
+import snownee.fruits.Fruits;
 import snownee.fruits.Hook;
-import snownee.kiwi.Kiwi;
 
 public class Connector implements IMixinConnector {
 
     @Override
     public void connect() {
-        Kiwi.logger.info("Invoking Mixin Connector");
+        Fruits.logger.info("Invoking Mixin Connector");
         Mixins.addConfiguration("assets/fruittrees/fruittrees.mixins.json");
         Hook.mixin = true;
     }
