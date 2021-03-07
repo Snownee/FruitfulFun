9da8772dfdc09333b6d323e7d1df6ff150e6807e
Snownee <mjy523@126.com>
2021-03-07 11:32:26 +0800

    fix: cherry door drops

--- src/main/java/snownee/fruits/cherry/block/SlidingDoorBlock.java
+++ src/main/java/snownee/fruits/cherry/block/SlidingDoorBlock.java
@@ -9,11 +9,9 @@ import net.minecraft.block.DoorBlock;
 import net.minecraft.block.material.Material;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.BlockItemUseContext;
-import net.minecraft.item.ItemStack;
 import net.minecraft.pathfinding.PathType;
 import net.minecraft.state.properties.DoorHingeSide;
 import net.minecraft.state.properties.DoubleBlockHalf;
-import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.ActionResultType;
 import net.minecraft.util.Direction;
 import net.minecraft.util.Hand;
@@ -60,29 +58,6 @@ public class SlidingDoorBlock extends DoorBlock {
         } else {
             return doubleblockhalf == DoubleBlockHalf.LOWER && facing == Direction.DOWN && !stateIn.isValidPosition(worldIn, currentPos) ? Blocks.AIR.getDefaultState() : super.updatePostPlacement(stateIn, facing, facingState, worldIn, currentPos, facingPos);
         }
-    }
-
-    @Override
-    public void harvestBlock(World worldIn, PlayerEntity player, BlockPos pos, BlockState state, @Nullable TileEntity te, ItemStack stack) {
-        super.harvestBlock(worldIn, player, pos, Blocks.AIR.getDefaultState(), te, stack);
-    }
-
-    @Override
-    public void onBlockHarvested(World worldIn, BlockPos pos, BlockState state, PlayerEntity player) {
-        DoubleBlockHalf doubleblockhalf = state.get(HALF);
-        BlockPos blockpos = doubleblockhalf == DoubleBlockHalf.LOWER ? pos.up() : pos.down();
-        BlockState blockstate = worldIn.getBlockState(blockpos);
-        if (blockstate.getBlock() == this && blockstate.get(HALF) != doubleblockhalf) {
-            worldIn.setBlockState(blockpos, Blocks.AIR.getDefaultState(), 35);
-            worldIn.playEvent(player, 2001, blockpos, Block.getStateId(blockstate));
-            ItemStack itemstack = player.getHeldItemMainhand();
-            if (!worldIn.isRemote && !player.isCreative() && player./*canHarvestBlock*/func_234569_d_(blockstate)) {
-                Block.spawnDrops(state, worldIn, pos, (TileEntity) null, player, itemstack);
-                Block.spawnDrops(blockstate, worldIn, blockpos, (TileEntity) null, player, itemstack);
-            }
-        }
-
-        super.onBlockHarvested(worldIn, pos, state, player);
     }
 
     @Override
