583bd3a5d9d5791f2d7beb583686180dddf01de4
Snownee <mjy523@126.com>
2021-12-20 03:24:18 +0800

    feat: finish food part. add and fix integration with Farmers Delight and Create (TODO: Druidcraft)

--- src/main/java/snownee/fruits/food/FoodModule.java
+++ src/main/java/snownee/fruits/food/FoodModule.java
@@ -1,11 +1,17 @@
 package snownee.fruits.food;
 
+import java.util.function.Supplier;
+
 import net.minecraft.item.Food;
 import net.minecraft.item.Item;
 import net.minecraft.item.Items;
+import net.minecraft.potion.Effect;
+import net.minecraft.potion.EffectInstance;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.common.crafting.CraftingHelper;
+import net.minecraftforge.common.util.Lazy;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.registries.ForgeRegistries;
 import snownee.kiwi.AbstractModule;
 import snownee.kiwi.Kiwi;
 import snownee.kiwi.KiwiModule;
@@ -16,11 +22,26 @@ import snownee.kiwi.KiwiModule;
 public class FoodModule extends AbstractModule {
 
 	public static final class Foods {
-		public static final Food GRAPEFRUIT_PANNA_COTTA = new Food.Builder().hunger(1).saturation(1).build();
-		public static final Food DONAUWELLE = new Food.Builder().hunger(1).saturation(1).build();
-		public static final Food HONEY_POMELO_TEA = new Food.Builder().hunger(1).saturation(1).build();
-		public static final Food RICE_WITH_FRUITS = new Food.Builder().hunger(1).saturation(1).build();
-		public static final Food LEMON_ROAST_CHICKEN = new Food.Builder().hunger(1).saturation(1).build();
+		private static Supplier<EffectInstance> makeSupplier(String id, int duration, int amplifier) {
+			return Lazy.of(() -> {
+				Effect effect = ForgeRegistries.POTIONS.getValue(new ResourceLocation(id));
+				if (effect == null) {
+					return null;
+				}
+				return new EffectInstance(effect, duration, amplifier);
+			});
+		}
+
+		private static final Supplier<EffectInstance> NOURISHED_PROVIDER = makeSupplier("farmersdelight:nourished", 6000, 0);
+		private static final Supplier<EffectInstance> COMFORT_PROVIDER = makeSupplier("farmersdelight:comfort", 3600, 0);
+		private static final Supplier<EffectInstance> REGENERATION_PROVIDER = makeSupplier("regeneration", 120, 0);
+		private static final Supplier<EffectInstance> SPEED_PROVIDER = makeSupplier("speed", 1200, 0);
+
+		public static final Food GRAPEFRUIT_PANNA_COTTA = new Food.Builder().hunger(14).saturation(1).effect(SPEED_PROVIDER, 1).build();
+		public static final Food DONAUWELLE = new Food.Builder().hunger(14).saturation(1).effect(REGENERATION_PROVIDER, 1).build();
+		public static final Food HONEY_POMELO_TEA = new Food.Builder().hunger(1).saturation(0.3F).effect(COMFORT_PROVIDER, 1).build();
+		public static final Food RICE_WITH_FRUITS = new Food.Builder().hunger(9).saturation(0.6F).effect(COMFORT_PROVIDER, 1).build();
+		public static final Food LEMON_ROAST_CHICKEN = new Food.Builder().hunger(12).saturation(0.9F).effect(NOURISHED_PROVIDER, 1).build();
 	}
 
 	public static final Item GRAPEFRUIT_PANNA_COTTA = new Item(itemProp().food(Foods.GRAPEFRUIT_PANNA_COTTA));
