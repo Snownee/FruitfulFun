4b9ab300dbb8192161a7b916295b5b2db10f7278
Snownee <mjy523@126.com>
2021-12-20 16:19:27 +0800

    feat: finish food part. add and fix integration with Farmers Delight and Create (TODO: Druidcraft)
    # Conflicts:
    #	src/main/java/snownee/fruits/food/FoodModule.java

--- src/main/java/snownee/fruits/food/FoodModule.java
+++ src/main/java/snownee/fruits/food/FoodModule.java
@@ -1,16 +1,17 @@
 package snownee.fruits.food;
 
+import java.util.function.Supplier;
+
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.effect.MobEffect;
+import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Items;
-import net.minecraftforge.common.crafting.CraftingHelper;
+import net.minecraftforge.common.util.Lazy;
+import net.minecraftforge.registries.ForgeRegistries;
 import snownee.kiwi.AbstractModule;
-import snownee.kiwi.Kiwi;
 import snownee.kiwi.KiwiModule;
-import snownee.kiwi.loader.event.InitEvent;
-import snownee.kiwi.recipe.AlternativesIngredientSerializer;
-import snownee.kiwi.recipe.TryParseCondition;
 
 @KiwiModule("food")
 @KiwiModule.Optional
@@ -18,11 +19,26 @@ import snownee.kiwi.recipe.TryParseCondition;
 public class FoodModule extends AbstractModule {
 
 	public static final class Foods {
-		public static final FoodProperties GRAPEFRUIT_PANNA_COTTA = new FoodProperties.Builder().nutrition(1).saturationMod(1).build();
-		public static final FoodProperties DONAUWELLE = new FoodProperties.Builder().nutrition(1).saturationMod(1).build();
-		public static final FoodProperties HONEY_POMELO_TEA = new FoodProperties.Builder().nutrition(1).saturationMod(1).build();
-		public static final FoodProperties RICE_WITH_FRUITS = new FoodProperties.Builder().nutrition(1).saturationMod(1).build();
-		public static final FoodProperties LEMON_ROAST_CHICKEN = new FoodProperties.Builder().nutrition(1).saturationMod(1).build();
+		private static Supplier<MobEffectInstance> makeSupplier(String id, int duration, int amplifier) {
+			return Lazy.of(() -> {
+				MobEffect effect = ForgeRegistries.MOB_EFFECTS.getValue(new ResourceLocation(id));
+				if (effect == null) {
+					return null;
+				}
+				return new MobEffectInstance(effect, duration, amplifier);
+			});
+		}
+
+		private static final Supplier<MobEffectInstance> NOURISHED_PROVIDER = makeSupplier("farmersdelight:nourished", 6000, 0);
+		private static final Supplier<MobEffectInstance> COMFORT_PROVIDER = makeSupplier("farmersdelight:comfort", 3600, 0);
+		private static final Supplier<MobEffectInstance> REGENERATION_PROVIDER = makeSupplier("regeneration", 120, 0);
+		private static final Supplier<MobEffectInstance> SPEED_PROVIDER = makeSupplier("speed", 1200, 0);
+
+		public static final FoodProperties GRAPEFRUIT_PANNA_COTTA = new FoodProperties.Builder().nutrition(14).saturationMod(1).effect(SPEED_PROVIDER, 1).build();
+		public static final FoodProperties DONAUWELLE = new FoodProperties.Builder().nutrition(14).saturationMod(1).effect(REGENERATION_PROVIDER, 1).build();
+		public static final FoodProperties HONEY_POMELO_TEA = new FoodProperties.Builder().nutrition(1).saturationMod(0.3F).effect(COMFORT_PROVIDER, 1).build();
+		public static final FoodProperties RICE_WITH_FRUITS = new FoodProperties.Builder().nutrition(9).saturationMod(0.6F).effect(COMFORT_PROVIDER, 1).build();
+		public static final FoodProperties LEMON_ROAST_CHICKEN = new FoodProperties.Builder().nutrition(12).saturationMod(0.9F).effect(NOURISHED_PROVIDER, 1).build();
 	}
 
 	public static final Item GRAPEFRUIT_PANNA_COTTA = new Item(itemProp().food(Foods.GRAPEFRUIT_PANNA_COTTA));
@@ -30,11 +46,5 @@ public class FoodModule extends AbstractModule {
 	public static final Item HONEY_POMELO_TEA = new Item(itemProp().food(Foods.HONEY_POMELO_TEA).craftRemainder(Items.GLASS_BOTTLE));
 	public static final Item RICE_WITH_FRUITS = new Item(itemProp().food(Foods.RICE_WITH_FRUITS));
 	public static final Item LEMON_ROAST_CHICKEN = new Item(itemProp().food(Foods.LEMON_ROAST_CHICKEN).craftRemainder(Items.BOWL));
-
-	@Override
-	protected void init(InitEvent event) {
-		CraftingHelper.register(TryParseCondition.Serializer.INSTANCE);
-		CraftingHelper.register(new ResourceLocation(Kiwi.MODID, "alternatives"), AlternativesIngredientSerializer.INSTANCE);
-	}
 
 }
