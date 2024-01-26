1acca406fa36ca313c855ffc55b7fabc6dcc7acc
Snownee <i@snownee.com>
2024-01-27 00:45:19 +0800

    feat: hide unfinished items in REI

--- src/main/java/snownee/fruits/compat/rei/FFREICompat.java
+++ src/main/java/snownee/fruits/compat/rei/FFREICompat.java
@@ -1,12 +1,14 @@
 package snownee.fruits.compat.rei;
 
 import dev.architectury.event.EventResult;
+import me.shedaniel.rei.api.client.entry.filtering.base.BasicFilteringRule;
 import me.shedaniel.rei.api.client.plugins.REIClientPlugin;
 import me.shedaniel.rei.api.client.registry.display.DisplayRegistry;
 import me.shedaniel.rei.api.common.category.CategoryIdentifier;
 import me.shedaniel.rei.api.common.display.Display;
 import me.shedaniel.rei.api.common.entry.EntryIngredient;
 import me.shedaniel.rei.api.common.entry.type.VanillaEntryTypes;
+import me.shedaniel.rei.api.common.util.EntryStacks;
 import me.shedaniel.rei.plugin.common.displays.brewing.BrewingRecipe;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
@@ -14,6 +16,9 @@ import net.minecraft.world.item.crafting.Ingredient;
 import snownee.fruits.Hooks;
 import snownee.fruits.bee.BeeModule;
 import snownee.fruits.bee.genetics.MutagenItem;
+import snownee.fruits.food.FoodModule;
+import snownee.fruits.pomegranate.PomegranateModule;
+import snownee.fruits.vacuum.VacModule;
 import snownee.lychee.compat.rei.REICompat;
 
 public class FFREICompat implements REIClientPlugin {
@@ -51,5 +56,18 @@ public class FFREICompat implements REIClientPlugin {
 				Ingredient.of(MutagenItem.BREWING_ITEM),
 				Ingredient.of(Items.POTION.getDefaultInstance()),
 				new ItemStack(BeeModule.MUTAGEN.get())));
+	}
+
+	@SuppressWarnings("UnstableApiUsage")
+	@Override
+	public void registerBasicEntryFiltering(BasicFilteringRule<?> rule) {
+		rule.hide(EntryStacks.of(PomegranateModule.POMEGRANATE.get()));
+		if (Hooks.food) {
+			rule.hide(EntryStacks.of(FoodModule.CHORUS_FRUIT_PIE.get()));
+		}
+		if (Hooks.vac) {
+			rule.hide(EntryStacks.of(VacModule.VAC_GUN.get()));
+			rule.hide(EntryStacks.of(VacModule.VAC_GUN_CASING.get()));
+		}
 	}
 }
