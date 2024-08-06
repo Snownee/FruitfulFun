e4eea70b21553a95df265e3e10503e5b648166ac
Snownee <i@snownee.com>
2024-08-06 13:21:55 +0800

    improve: mixin
    # Conflicts:
    #	src/main/java/snownee/fruits/mixin/BeeHiveBlockEntityMixin.java

--- src/main/java/snownee/fruits/mixin/BeeHiveBlockEntityMixin.java
+++ src/main/java/snownee/fruits/mixin/BeeHiveBlockEntityMixin.java
@@ -1,41 +1,22 @@
 package snownee.fruits.mixin;
 
-import java.util.List;
-
-import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.llamalad7.mixinextras.sugar.Local;
 
-import net.minecraft.core.BlockPos;
-import net.minecraft.world.entity.Entity;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BeehiveBlockEntity;
-import net.minecraft.world.level.block.state.BlockState;
 
 @Mixin(BeehiveBlockEntity.class)
 public class BeeHiveBlockEntityMixin {
-	@ModifyExpressionValue(method = "releaseOccupant", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/Level;isRaining()Z"))
-	private static boolean releaseOccupantSuppressDefault(boolean original) {
-		return false;
-	}
-
-	@Inject(method = "releaseOccupant", at = @At("HEAD"), cancellable = true)
-	private static void releaseOccupant(
-			Level level,
-			BlockPos blockPos,
-			BlockState blockState,
-			BeehiveBlockEntity.BeeData beeData,
-			@Nullable List<Entity> list,
-			BeehiveBlockEntity.BeeReleaseStatus beeReleaseStatus,
-			@Nullable BlockPos blockPos2,
-			CallbackInfoReturnable<Boolean> ci) {
-		if (beeReleaseStatus != BeehiveBlockEntity.BeeReleaseStatus.EMERGENCY && level.isRaining() && !beeData.entityData.getBoolean(
-				"RainCapable")) {
-			ci.setReturnValue(false);
-		}
+	@WrapOperation(method = "releaseOccupant", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/Level;isRaining()Z"))
+	private static boolean releaseOccupantSuppressDefault(
+			Level instance,
+			Operation<Boolean> original,
+			@Local(argsOnly = true) BeehiveBlockEntity.BeeData beeData) {
+		return original.call(instance) && !beeData.entityData.getBoolean("RainCapable");
 	}
 }
