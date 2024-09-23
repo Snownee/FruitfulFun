bd87010438a5221d5357c746d99b75d6c97e54c2
Snownee <i@snownee.com>
2024-09-23 13:51:49 +0800

    feat: interact with beehive to let leashed bees enter the block

--- src/main/java/snownee/fruits/mixin/bee/BeehiveBlockMixin.java
+++ src/main/java/snownee/fruits/mixin/bee/BeehiveBlockMixin.java
@@ -12,17 +12,25 @@ import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.llamalad7.mixinextras.sugar.Local;
 
+import net.minecraft.core.BlockPos;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.animal.Bee;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraft.world.item.enchantment.Enchantments;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.BeehiveBlock;
 import net.minecraft.world.level.block.entity.BeehiveBlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.storage.loot.LootParams;
 import net.minecraft.world.level.storage.loot.parameters.LootContextParams;
+import net.minecraft.world.phys.AABB;
+import net.minecraft.world.phys.BlockHitResult;
 import snownee.fruits.bee.BeeAttributes;
 import snownee.fruits.bee.genetics.Trait;
 import snownee.fruits.duck.FFBeehiveBlockEntity;
@@ -66,5 +74,43 @@ public class BeehiveBlockMixin {
 			itemStack.enchant(Enchantments.SILK_TOUCH, 1);
 			builder.withParameter(LootContextParams.TOOL, itemStack);
 		}
+	}
+
+	@Inject(method = "use", at = @At("HEAD"), cancellable = true)
+	private void use(
+			BlockState blockState,
+			Level level,
+			BlockPos pos,
+			Player player,
+			InteractionHand hand,
+			BlockHitResult hit,
+			CallbackInfoReturnable<InteractionResult> cir) {
+		if (!(level.getBlockEntity(pos) instanceof BeehiveBlockEntity be)) {
+			return;
+		}
+		boolean bl = false;
+		int i = pos.getX();
+		int j = pos.getY();
+		int k = pos.getZ();
+		// https://bugs.mojang.com/projects/MC/issues/MC-276925
+		// Distance limit of using lead on a fence is directional
+		List<Bee> bees = level.getEntitiesOfClass(Bee.class, new AABB(i - 7, j - 7, k - 7, i + 8, j + 8, k + 8));
+		for (Bee bee : bees) {
+			if (bee.getLeashHolder() != player) {
+				continue;
+			}
+			if (!level.isClientSide && !be.isFull()) {
+				bee.dropLeash(true, true);
+				be.addOccupant(bee, bee.hasNectar());
+			}
+			bl = true;
+		}
+		if (!bl) {
+			return;
+		}
+		if (!level.isClientSide) {
+			level.playSound(null, pos, SoundEvents.LEASH_KNOT_PLACE, player.getSoundSource(), 1, 1);
+		}
+		cir.setReturnValue(InteractionResult.sidedSuccess(level.isClientSide));
 	}
 }
