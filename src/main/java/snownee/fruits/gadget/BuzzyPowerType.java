e3f39fba40870b8df13c0502009eaee5985532e0
Snownee <i@snownee.com>
2026-08-21 20:23:10 +0800

    fix: accidentally import jade classes

--- src/main/java/snownee/fruits/gadget/BuzzyPowerType.java
+++ src/main/java/snownee/fruits/gadget/BuzzyPowerType.java
@@ -2,15 +2,20 @@ package snownee.fruits.gadget;
 
 import java.util.Locale;
 
-import snownee.jade.api.SimpleStringRepresentable;
+import net.minecraft.util.StringRepresentable;
 
-public enum BuzzyPowerType implements SimpleStringRepresentable {
+public enum BuzzyPowerType implements StringRepresentable {
 	RED, GREEN, BLUE;
 
 	private final String name = name().toLowerCase(Locale.ENGLISH);
 
 	@Override
 	public String toString() {
+		return name;
+	}
+
+	@Override
+	public String getSerializedName() {
 		return name;
 	}
 }
