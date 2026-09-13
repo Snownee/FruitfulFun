76245b0b1f003918a5e48ed0a71453fda1fe3e6a
Snownee <i@snownee.com>
2026-09-14 03:47:01 +0800

    feat: guidebook part 2

--- src/main/java/snownee/fruits/datagen/guide/FruitfulFunBook.java
+++ src/main/java/snownee/fruits/datagen/guide/FruitfulFunBook.java
@@ -1,7 +1,5 @@
 package snownee.fruits.datagen.guide;
 
-import static snownee.fruits.datagen.guide.GuideUtil.lines;
-
 import com.klikli_dev.modonomicon.api.datagen.SingleBookSubProvider;
 import com.klikli_dev.modonomicon.api.datagen.book.BookModel;
 import com.klikli_dev.modonomicon.book.BookDisplayMode;
@@ -19,7 +17,8 @@ public class FruitfulFunBook extends SingleBookSubProvider {
 		return book.withDisplayMode(BookDisplayMode.INDEX)
 				.withGenerateBookItem(false)
 				.withCustomBookItem(modLoc("guide"))
-				.withShowRecentlyUnlocked(false);
+				.withShowRecentlyUnlocked(false)
+				.withTheme(theme -> theme.withId(modLoc("theme")));
 	}
 
 	@Override
@@ -37,23 +36,11 @@ public class FruitfulFunBook extends SingleBookSubProvider {
 
 	@Override
 	protected String bookName() {
-		return "果实乐趣指南";
+		return "book.fruitfulfun.guide.name";
 	}
 
 	@Override
 	protected String bookTooltip() {
-		return "一本记录水果、蜜蜂与仪式的指南。";
-	}
-
-	@Override
-	protected String bookDescription() {
-		return lines("""
-				欢迎阅读《果实乐趣指南》！
-
-				这本书记录了 Fruitful Fun 模组的种种玩法：
-				果树、养蜂、仪式、工具与烹饪。
-
-				有些内容需要你亲自去发现。
-				""");
+		return "book.fruitfulfun.guide.tooltip";
 	}
 }
